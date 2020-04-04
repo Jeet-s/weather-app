058e411ccdc5c57afd28dcf309d9b03b78b4f6a1
jeet-s <jagjeet@simplifiedautomation.ca>
2020-04-04 20:53:47 +0530

    fetched static weather data from open weather api

---
+++ app.js
@@ -0,0 +1,8 @@
+const request = require('request');
+
+const url = 'http://api.openweathermap.org/data/2.5/weather?lat=28.644800&lon=77.216721&appid=6cf9805fc6f47449dbded7516ccd78fd';
+
+request({ url: url }, (error, response) => {
+    const data = JSON.parse(response.body);
+    console.log(`It is currently ${data.main.temp} degrees out`);
+});
