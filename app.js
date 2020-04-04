dfe0efe37bc950bd8fddbdbb899ca352974dac5d
jeet-s <jagjeet@simplifiedautomation.ca>
2020-04-04 21:01:01 +0530

    changed temp unit to celsius

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const request = require('request');
 
-const url = 'http://api.openweathermap.org/data/2.5/weather?lat=28.644800&lon=77.216721&appid=6cf9805fc6f47449dbded7516ccd78fd';
+const url = 'http://api.openweathermap.org/data/2.5/weather?lat=28.644800&lon=77.216721&appid=6cf9805fc6f47449dbded7516ccd78fd&units=metric';
 
 request({ url: url }, (error, response) => {
     const data = JSON.parse(response.body);
-    console.log(`It is currently ${data.main.temp} degrees out`);
+    console.log(`It is currently ${data.main.temp} degree Celsius out`);
 });
