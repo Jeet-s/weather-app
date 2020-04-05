703b18bdf2ac8d21ec3bac38741666730f4c5de8
jeet-s <jagjeet@simplifiedautomation.ca>
2020-04-05 21:00:44 +0530

    added iput field for getting location and displayed temp

--- public/js/index.js
+++ public/js/index.js
@@ -1,16 +1,23 @@
 weatherForm = document.querySelector('form');
 locationInput = document.querySelector('input');
+messageOne = document.querySelector('.message-1');
+messageTwo = document.querySelector('.message-2');
 
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault();
+    messageOne.textContent = 'Loading...';
+    messageTwo.textContent = '';
     fetch(`http://localhost:3000/weather?address=${locationInput.value}`)
         .then((response) => {
-            if (response.error) {
-                return console.log(response.error)
-            }
-            response.json().then((data) => {
-                console.log(data);
-            })
+                response.json().then((data) => {
+                    if (data.error) {
+                        messageOne.innerHTML = '<span class="error">' + data.error + '</span>';
+                        messageTwo.textContent = '';
+                    } else {
+                        messageOne.innerHTML = '<span class="temp">' + data.forcast.main.temp + '<sup>&#176</sup>C' + '</span>';
+                        messageTwo.innerHTML = '<span class="location">' + data.location + '</span>';
+                    }
+                })
         })
         .catch(error => {
             console.log(error)
